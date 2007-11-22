71ef6782e82b6d9899e0021e0200c6f95cd60c40
joa23 <joa23@93fdad72-463b-497d-9b16-63e3313eb2ee>
2007-11-22 00:34:14 +0000

    + fixing [ 1496904 ] Creating new directory does not work! + add jmock 2.1 as dependency since it is sooo much easier to use.

--- src/main/java/net/sf/webdav/methods/DoHead.java
+++ src/main/java/net/sf/webdav/methods/DoHead.java
@@ -9,7 +9,8 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -29,114 +30,130 @@ import java.io.IOException;
 
 public class DoHead extends AbstractMethod {
 
-    protected int debug;
-    protected String dftIndexFile;
-    protected WebdavStore store;
-    protected String insteadOf404;
-    protected ResourceLocks resLocks;
-    protected MimeTyper mimeTyper;
-    protected int contLength;
+	protected int debug;
+	protected String dftIndexFile;
+	protected WebdavStore store;
+	protected String insteadOf404;
+	protected ResourceLocks resLocks;
+	protected MimeTyper mimeTyper;
+	protected int contLength;
 
-    public DoHead(WebdavStore store, String dftIndexFile, String insteadOf404, ResourceLocks resourceLocks, MimeTyper mimeTyper, int contentLengthHeader, int debug) {
-        this.store = store;
-        this.dftIndexFile = dftIndexFile;
-        this.insteadOf404 = insteadOf404;
-        this.resLocks = resourceLocks;
-        this.mimeTyper = mimeTyper;
-        this.contLength = contentLengthHeader;
-        this.debug = debug;
-    }
+	public DoHead(WebdavStore store, String dftIndexFile, String insteadOf404,
+			ResourceLocks resourceLocks, MimeTyper mimeTyper,
+			int contentLengthHeader, int debug) {
+		this.store = store;
+		this.dftIndexFile = dftIndexFile;
+		this.insteadOf404 = insteadOf404;
+		this.resLocks = resourceLocks;
+		this.mimeTyper = mimeTyper;
+		this.contLength = contentLengthHeader;
+		this.debug = debug;
+	}
 
+	public void execute(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
 
-    public void execute(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String path = getRelativePath(req);
-        if (debug == 1)
-            System.err.println("-- do " + req.getMethod());
+		// determines if the uri exists.
 
-        if (store.isFolder(path)) {
-            if (dftIndexFile != null && !dftIndexFile.trim().equals("")) {
-                resp.sendRedirect(resp.encodeRedirectURL(req.getRequestURI()
-                        + this.dftIndexFile));
-                return;
-            }
-        }
-        if (!store.objectExists(path)) {
-            if (this.insteadOf404 != null && !insteadOf404.trim().equals("")) {
-                path = this.insteadOf404;
-            }
-        }
+		boolean bUriExists = false;
 
-        String lockOwner = "doGet" + System.currentTimeMillis()
-                + req.toString();
+		String path = getRelativePath(req);
+		if (debug == 1)
+			System.err.println("-- do " + req.getMethod());
 
-        if (resLocks.lock(path, lockOwner, false, 0)) {
-            try {
+		if (store.isFolder(path)) {
+			bUriExists = true;
+			if (dftIndexFile != null && !dftIndexFile.trim().equals("")) {
 
-                if (store.isResource(path)) {
-                    // path points to a file but ends with / or \
-                    if (path.endsWith("/") || (path.endsWith("\\"))) {
-                        resp.sendError(HttpServletResponse.SC_NOT_FOUND, req
-                                .getRequestURI());
-                    } else {
+				resp.sendRedirect(resp.encodeRedirectURL(req.getRequestURI()
+						+ this.dftIndexFile));
+				return;
+			}
+		}
+		if (!store.objectExists(path)) {
 
-                        // setting headers
-                        long lastModified = store.getLastModified(path)
-                                .getTime();
-                        resp.setDateHeader("last-modified", lastModified);
+			if (this.insteadOf404 != null && !insteadOf404.trim().equals("")) {
+				path = this.insteadOf404;
+			}
 
-                        long resourceLength = store.getResourceLength(path);
+		} else
+			bUriExists = true;
 
-                        if (contLength == 1) {
-                            if (resourceLength > 0) {
-                                if (resourceLength <= Integer.MAX_VALUE) {
-                                    resp.setContentLength((int) resourceLength);
-                                } else {
-                                    resp.setHeader("content-length", ""
-                                            + resourceLength);
-                                    // is "content-length" the right header?
-                                    // is
-                                    // long
-                                    // a valid format?
-                                }
-                            }
-                        }
+		String lockOwner = "doGet" + System.currentTimeMillis()
+				+ req.toString();
 
-                        String mimeType = mimeTyper.getMimeType(path);
-                        if (mimeType != null) {
-                            resp.setContentType(mimeType);
-                        } else {
-                            int lastSlash = path.replace('\\', '/')
-                                    .lastIndexOf('/');
-                            int lastDot = path.indexOf(".", lastSlash);
-                            if (lastDot == -1) {
-                                resp.setContentType("text/html");
-                            }
-                        }
+		if (resLocks.lock(path, lockOwner, false, 0)) {
+			try {
 
-                        doBody(resp, path);
-                    }
-                } else {
-                    folderBody(path, resp, req);
-                }
-            } catch (AccessDeniedException e) {
-                resp.sendError(WebdavStatus.SC_FORBIDDEN);
-            } catch (ObjectAlreadyExistsException e) {
-                resp.sendError(WebdavStatus.SC_NOT_FOUND, req.getRequestURI());
-            } catch (WebdavException e) {
-                resp.sendError(WebdavStatus.SC_INTERNAL_SERVER_ERROR);
-            } finally {
-                resLocks.unlock(path, lockOwner);
-            }
-        } else {
-            resp.sendError(WebdavStatus.SC_INTERNAL_SERVER_ERROR);
-        }
-    }
+				if (store.isResource(path)) {
+					// path points to a file but ends with / or \
+					if (path.endsWith("/") || (path.endsWith("\\"))) {
+						resp.sendError(HttpServletResponse.SC_NOT_FOUND, req
+								.getRequestURI());
+					} else {
 
-    protected void folderBody(String path, HttpServletResponse resp, HttpServletRequest req) throws IOException {
-        // no body for HEAD
-    }
+						// setting headers
+						long lastModified = store.getLastModified(path)
+								.getTime();
+						resp.setDateHeader("last-modified", lastModified);
 
-    protected void doBody(HttpServletResponse resp, String path) throws IOException {
-        // no body for HEAD
-    }
+						long resourceLength = store.getResourceLength(path);
+
+						if (contLength == 1) {
+							if (resourceLength > 0) {
+								if (resourceLength <= Integer.MAX_VALUE) {
+									resp.setContentLength((int) resourceLength);
+								} else {
+									resp.setHeader("content-length", ""
+											+ resourceLength);
+									// is "content-length" the right header?
+									// is
+									// long
+									// a valid format?
+								}
+							}
+						}
+
+						String mimeType = mimeTyper.getMimeType(path);
+						if (mimeType != null) {
+							resp.setContentType(mimeType);
+						} else {
+							int lastSlash = path.replace('\\', '/')
+									.lastIndexOf('/');
+							int lastDot = path.indexOf(".", lastSlash);
+							if (lastDot == -1) {
+								resp.setContentType("text/html");
+							}
+						}
+
+						doBody(resp, path);
+					}
+				} else {
+					folderBody(path, resp, req);
+				}
+			} catch (AccessDeniedException e) {
+				resp.sendError(WebdavStatus.SC_FORBIDDEN);
+			} catch (ObjectAlreadyExistsException e) {
+				resp.sendError(WebdavStatus.SC_NOT_FOUND, req.getRequestURI());
+			} catch (WebdavException e) {
+				resp.sendError(WebdavStatus.SC_INTERNAL_SERVER_ERROR);
+			} finally {
+				resLocks.unlock(path, lockOwner);
+			}
+		} else {
+			resp.sendError(WebdavStatus.SC_INTERNAL_SERVER_ERROR);
+		}
+		if (!bUriExists)
+			resp.setStatus(WebdavStatus.SC_NOT_FOUND);
+	}
+
+	protected void folderBody(String path, HttpServletResponse resp,
+			HttpServletRequest req) throws IOException {
+		// no body for HEAD
+	}
+
+	protected void doBody(HttpServletResponse resp, String path)
+			throws IOException {
+		// no body for HEAD
+	}
 }
--- src/test/java/net/sf/webdav/methods/DoHeadTest.java
+++ src/test/java/net/sf/webdav/methods/DoHeadTest.java
@@ -1,120 +1,136 @@
 package net.sf.webdav.methods;
 
-import org.jmock.MockObjectTestCase;
-import org.jmock.Mock;
-import net.sf.webdav.WebdavStore;
-import net.sf.webdav.MimeTyper;
-import net.sf.webdav.ResourceLocks;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
-import java.io.ByteArrayInputStream;
+
+import net.sf.webdav.MimeTyper;
+import net.sf.webdav.ResourceLocks;
+import net.sf.webdav.WebdavStore;
+
+import org.jmock.Expectations;
+import org.jmock.Mock;
+import org.jmock.MockObjectTestCase;
+import org.jmock.Mockery;
 
 public class DoHeadTest extends MockObjectTestCase {
 
-    Mock mockStore;
-    Mock mockMimeTyper;
-    Mock mockReq;
-    Mock mockRes;
+	Mock mockStore;
+	Mock mockMimeTyper;
+	Mock mockReq;
+	Mock mockRes;
 
-    protected void setUp() throws Exception {
-        mockStore = mock(WebdavStore.class);
-        mockMimeTyper = mock(MimeTyper.class);
-        mockReq = mock(HttpServletRequest.class);
-        mockRes = mock(HttpServletResponse.class);
-    }
+	protected void setUp() throws Exception {
+		mockStore = mock(WebdavStore.class);
+		mockMimeTyper = mock(MimeTyper.class);
+		mockReq = mock(HttpServletRequest.class);
+		mockRes = mock(HttpServletResponse.class);
+	}
 
-    public void testAccessOfaMissingPageResultsIn404() throws Exception {
+	public void testAccessOfaMissingPageResultsIn404() throws Exception {
 
-        mockStore.expects(once()).method("isFolder").with(
-                eq("/index.html")).will(returnValue(false));
-        mockStore.expects(once()).method("objectExists").with(
-                eq("/index.html")).will(returnValue(false));
-        mockStore.expects(once()).method("isResource").with(eq("/index.html"))
-                .will(returnValue(false));
+		Mockery mockery = new Mockery();
+		final WebdavStore webdavStore = mockery.mock(WebdavStore.class);
+		MimeTyper mimeTyper = mockery.mock(MimeTyper.class);
+		final HttpServletRequest request = mockery
+				.mock(HttpServletRequest.class);
+		final HttpServletResponse response = mockery
+				.mock(HttpServletResponse.class);
+		mockery.checking(new Expectations() {
+			{
+				String indexHtml = "/index.html";
+				one(webdavStore).isFolder(indexHtml);
+				will(returnValue(false));
+				one(webdavStore).objectExists(indexHtml);
+				will(returnValue(false));
 
-        DoHead doHead = new DoHead((WebdavStore) mockStore.proxy(), null, null,
-                new ResourceLocks(), (MimeTyper) mockMimeTyper.proxy(), 0, 0);
+				one(webdavStore).isResource(indexHtml);
+				will(returnValue(false));
 
-        mockReq.expects(once()).method("getAttribute").with(
-                eq("javax.servlet.include.request_uri"))
-                .will(returnValue(null));
+				one(request).getAttribute(
+						"javax.servlet.include.request_uri");
+				will(returnValue(null));
+				one(request).getPathInfo();
+				will(returnValue(indexHtml));
+				one(response).setStatus(404);
+			}
+		});
 
-        mockReq.expects(once()).method("getPathInfo").withNoArguments().will(
-                returnValue("/index.html"));
+		DoHead doHead = new DoHead(webdavStore, null, null,
+				new ResourceLocks(), mimeTyper, 0, 0);
+		doHead.execute(request, response);
+		// TODO should this return something to the browser ?
+		mockery.assertIsSatisfied();
+	}
 
-        doHead.execute((HttpServletRequest) mockReq.proxy(),
-                (HttpServletResponse) mockRes.proxy());
+	public void testAccessOfaPageResultsInPage() throws Exception {
 
-        // TODO should this return something to the browser ?
+		mockStore.expects(once()).method("isFolder").with(eq("/index.html"))
+				.will(returnValue(false));
 
-    }
+		// there is something extraneous about this objectExists() invocation.
+		// Whether true or
+		// false it does not affect the outcome
 
+		mockStore.expects(once()).method("objectExists")
+				.with(eq("/index.html")).will(returnValue(true));
+		mockStore.expects(once()).method("isResource").with(eq("/index.html"))
+				.will(returnValue(true));
+		Date now = new Date(System.currentTimeMillis());
+		mockStore.expects(once()).method("getLastModified").with(
+				eq("/index.html")).will(returnValue(now));
+		mockStore.expects(once()).method("getResourceLength").with(
+				eq("/index.html")).will(returnValue(8L));
 
-    public void testAccessOfaPageResultsInPage() throws Exception {
+		mockMimeTyper.expects(once()).method("getMimeType").with(
+				eq("/index.html")).will(returnValue("text/foo"));
 
-        mockStore.expects(once()).method("isFolder").with(
-                eq("/index.html")).will(returnValue(false));
+		DoHead doHead = new DoHead((WebdavStore) mockStore.proxy(), null, null,
+				new ResourceLocks(), (MimeTyper) mockMimeTyper.proxy(), 0, 0);
 
-        // there is something extraneous about this objectExists() invocation. Whether true or
-        // false it does not affect the outcome
+		mockReq.expects(once()).method("getAttribute").with(
+				eq("javax.servlet.include.request_uri"))
+				.will(returnValue(null));
 
-        mockStore.expects(once()).method("objectExists").with(
-                eq("/index.html")).will(returnValue(true));
-        mockStore.expects(once()).method("isResource").with(eq("/index.html"))
-                .will(returnValue(true));
-        Date now = new Date(System.currentTimeMillis());
-        mockStore.expects(once()).method("getLastModified").with(eq("/index.html"))
-                .will(returnValue(now));
-        mockStore.expects(once()).method("getResourceLength").with(eq("/index.html"))
-                .will(returnValue(8L));
+		mockReq.expects(once()).method("getPathInfo").withNoArguments().will(
+				returnValue("/index.html"));
 
-        mockMimeTyper.expects(once()).method("getMimeType").with(eq("/index.html")).will(returnValue("text/foo"));
+		mockRes.expects(once()).method("setDateHeader").with(
+				eq("last-modified"), eq(now.getTime()));
+		mockRes.expects(once()).method("setContentType").with(eq("text/foo"));
 
-        DoHead doHead = new DoHead((WebdavStore) mockStore.proxy(), null, null,
-                new ResourceLocks(), (MimeTyper) mockMimeTyper.proxy(),0, 0);
+		doHead.execute((HttpServletRequest) mockReq.proxy(),
+				(HttpServletResponse) mockRes.proxy());
 
-        mockReq.expects(once()).method("getAttribute").with(
-                eq("javax.servlet.include.request_uri"))
-                .will(returnValue(null));
+	}
 
-        mockReq.expects(once()).method("getPathInfo").withNoArguments().will(
-                returnValue("/index.html"));
+	public void testAccessOfaDirectoryResultsInRedirectIfDefaultIndexFilePresent()
+			throws Exception {
 
-        mockRes.expects(once()).method("setDateHeader").with(eq("last-modified"), eq(now.getTime()));
-        mockRes.expects(once()).method("setContentType").with(eq("text/foo"));
+		mockStore.expects(once()).method("isFolder").with(eq("/foo/")).will(
+				returnValue(true));
 
-        doHead.execute((HttpServletRequest) mockReq.proxy(),
-                (HttpServletResponse) mockRes.proxy());
+		DoHead doHead = new DoHead((WebdavStore) mockStore.proxy(), "/yeehaaa",
+				null, new ResourceLocks(), (MimeTyper) mockMimeTyper.proxy(),
+				0, 0);
 
-    }
+		mockReq.expects(once()).method("getAttribute").with(
+				eq("javax.servlet.include.request_uri"))
+				.will(returnValue(null));
 
+		mockReq.expects(once()).method("getPathInfo").withNoArguments().will(
+				returnValue("/foo/"));
+		mockReq.expects(once()).method("getRequestURI").withNoArguments().will(
+				returnValue("/foo"));
 
-    public void testAccessOfaDirectoryResultsInRedirectIfDefaultIndexFilePresent() throws Exception {
+		mockRes.expects(once()).method("encodeRedirectURL").with(
+				eq("/foo/yeehaaa"));
+		mockRes.expects(once()).method("sendRedirect").with(eq(null));
 
-        mockStore.expects(once()).method("isFolder").with(
-                eq("/foo/")).will(returnValue(true));
+		doHead.execute((HttpServletRequest) mockReq.proxy(),
+				(HttpServletResponse) mockRes.proxy());
 
-        DoHead doHead = new DoHead((WebdavStore) mockStore.proxy(), "/yeehaaa", null,
-                new ResourceLocks(), (MimeTyper) mockMimeTyper.proxy(), 0, 0);
-
-        mockReq.expects(once()).method("getAttribute").with(
-                eq("javax.servlet.include.request_uri"))
-                .will(returnValue(null));
-
-        mockReq.expects(once()).method("getPathInfo").withNoArguments().will(
-                returnValue("/foo/"));
-        mockReq.expects(once()).method("getRequestURI").withNoArguments().will(
-                returnValue("/foo"));
-
-        mockRes.expects(once()).method("encodeRedirectURL").with(eq("/foo/yeehaaa"));
-        mockRes.expects(once()).method("sendRedirect").with(eq(null));
-
-        doHead.execute((HttpServletRequest) mockReq.proxy(),
-                (HttpServletResponse) mockRes.proxy());
-
-
-    }
+	}
 
 }
