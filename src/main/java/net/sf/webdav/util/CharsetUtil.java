2c926930922c6050ea32d772e8ead2b0865dc145
Ellipsis <info@ellipsis.nl>
2018-01-12 13:11:55 +0100

    CharsetUtil

---
+++ src/main/java/net/sf/webdav/util/CharsetUtil.java
@@ -0,0 +1,21 @@
+package net.sf.webdav.util;
+
+public class CharsetUtil {
+	public static final char CHAR_ASTERIX = '*';
+	public static final char CHAR_COLON = ':';
+	public static final char CHAR_DASH = '-';
+	public static final char CHAR_DOT = '.';
+	public static final char CHAR_EQUALS = '=';
+	public static final char CHAR_FORWARD_SLASH = '/';
+	public static final char CHAR_GREATER_THAN = '>';
+	public static final char CHAR_LEFT_PARENTHESIS = '(';
+	public static final char CHAR_LESS_THAN = '<';
+	public static final char CHAR_RIGHT_PARENTHESIS = ')';
+	public static final char CHAR_SPACE = ' ';
+	public static final char CHAR_UNDERSCORE = '_';
+		
+	public static final String DQUOTE = "\"";
+	public static final String FORWARD_SLASH = "/";
+	public static final String GREATER_THAN = ">";
+	public static final String LESS_THAN = "<";
+}
